5303322e8eb11018845f08e8acbef4ce89b56303
SeniorDmitriy <dimaponomarenko443@gmail.com>
2022-09-21 12:27:33 +0300

    added social in footer

--- src/view/components/Main/Footer/index.tsx
+++ src/view/components/Main/Footer/index.tsx
@@ -1,8 +1,8 @@
 // Core
-import React, { FC } from 'react';
+import React, { FC, useRef } from 'react';
 
-// Bus
-// import {} from '../../../bus/'
+// Assets
+import { svgLinkedinIcon, svgMailIcon, svgGithubIcon } from '../../../../assets/images';
 
 // Styles
 import * as S from './styles';
@@ -13,22 +13,80 @@ type PropTypes = {
 }
 
 export const Footer: FC<PropTypes> = () => {
+    const ref = useRef<HTMLSpanElement>(null);
+
+    const alertText = () => {
+        setTimeout(() => {
+            if (ref.current) {
+                console.log('start');
+
+                ref.current.style.display = 'none';
+            }
+        }, 2000);
+    };
+
     return (
         <S.Container>
             <S.Inner>
-                {/* <S.List>
+                <S.List>
                     <S.Item>
-                        <S.Link href = '#'>
-                            gmail
-                        </S.Link>
-                        <S.Link href = '#'>
-                            github
-                        </S.Link>
-                        <S.Link href = '#'>
-                            LinkedIn
+                        <S.Link href = 'https://www.linkedin.com/in/dimaponomart683/'>
+                            <img
+                                alt = 'linkedIn icon'
+                                src = { svgLinkedinIcon }
+                            />
+                            <span>
+                                Linkedin
+                            </span>
                         </S.Link>
                     </S.Item>
-                </S.List> */}
+                    <S.Item>
+                        <S.Link href = 'https://github.com/DmitryPonomarenko34'>
+                            <img
+                                alt = 'linkedIn icon'
+                                src = { svgGithubIcon }
+                            />
+                            <span>
+                                Developer
+                            </span>
+                        </S.Link>
+                    </S.Item>
+                    <S.Item>
+                        <S.Link
+                            href = 'https://github.com/Arziburst'>
+                            <img
+                                alt = 'linkedIn icon'
+                                src = { svgGithubIcon }
+                            />
+                            <span>
+                                Developer
+                            </span>
+                        </S.Link>
+                    </S.Item>
+                    <S.Item>
+                        <S.Link
+                            href = '#'
+                            onClick = { (event) => {
+                                event.preventDefault();
+                                navigator.clipboard.writeText('dimaponomarenko443@gmail.com');
+                                if (ref.current) {
+                                    ref.current.style.display = 'block   ';
+                                }
+                                alertText();
+                            } }>
+                            <img
+                                alt = 'linkedIn icon'
+                                src = { svgMailIcon }
+                            />
+                            <S.SuccesCopy ref = { ref }>
+                                Text succes copy
+                            </S.SuccesCopy>
+                            <span>
+                                Gmail
+                            </span>
+                        </S.Link>
+                    </S.Item>
+                </S.List>
                 <S.Copyright>
                     © 2022 ResumeCreator
                 </S.Copyright>
--- src/view/components/Main/Footer/styles.ts
+++ src/view/components/Main/Footer/styles.ts
@@ -2,9 +2,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px 15px;
   background-color: #fff;
 `;
 
@@ -12,20 +9,38 @@ export const Inner = styled.div`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px 15px;
 `;
 
 export const List = styled.ul`
   display: block;
+  margin-bottom: 40px;
 `;
 
 export const Item = styled.li`
+  margin-bottom: 5px;
 `;
 
 export const Link = styled.a`
-  display: block;
+  display: flex;
+  align-items: center;
+  color: #262b34;
+  img {
+    width: 24px;
+    height: 24px;
+    margin-right: 10px;
+  }
 `;
 
 export const Copyright = styled.p`
   width: 100%;
+  text-align: center;
+`;
+
+export const SuccesCopy = styled.span`
+  width: 100%;
+  display: none;
   text-align: center;
 `;
