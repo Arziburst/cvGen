3f520fe0f68a83c9f0f0bb6cf8fe3cba2ea06a3c
SeniorDmitry <dimaponomarenko443@gmail.com>
2022-09-29 00:27:39 +0300

    start added heading for input

--- src/view/elements/debounceInput.tsx
+++ src/view/elements/debounceInput.tsx
@@ -6,9 +6,17 @@ import { DebounceInput } from 'react-debounce-input';
 // Constant
 import { WAIT_TIME } from '../../init';
 
+// Types
+type propsType = {
+    handleChangeFunc: (event: ChangeEvent<HTMLInputElement>) => void;
+    placeholder: string;
+    value: string;
+}
 // Styled
-export const InputBox = styled.div`
+export const InputBox = styled.label`
   width: 100%;
+  position: relative;
+
   input {
     border: 1px solid transparent;
     background-color: transparent;
@@ -20,13 +28,7 @@ export const InputBox = styled.div`
     &:focus,
     &:hover {
       border-color: ${({ theme }) => theme.main.colorSecond};
-      outline-color: ${({ theme }) => theme.main.colorSecond};
-    }
-
-    &::placeholder {
-      font-size: inherit;
-      color: inherit;
-      opacity: 0.8;
+      outline-color: transparent;
     }
   }
 
@@ -36,19 +38,40 @@ export const InputBox = styled.div`
     }
   }
 `;
+export const DecorText = styled.p<{value: string}>`
+  font-size: inherit;
+  color: #fff;
+  position: absolute;
+  left: 15px;
+  top: -9px;
+  span {
+    position: relative;
+    z-index: 4;
+  }
+  &::before {
+    content: '';
+    position: absolute;
+    opacity: 0;
+    z-index: 3;
+    left: -1px;
+    top: 7px;
+    width: calc(100% + 4px);
+    height: 3px;
+    background-color: #384052;
+  }
 
-type propsType = {
-    handleChangeFunc: (event: ChangeEvent<HTMLInputElement>) => void;
-    placeholder: string;
-    value: string;
-}
+`;
 
 export const AppDebounceInput: FC<propsType> = ({ handleChangeFunc, placeholder, value }) => {
     return (
         <InputBox>
+            <DecorText value = { value }>
+                <span>
+                    {placeholder}
+                </span>
+            </DecorText>
             <DebounceInput
                 debounceTimeout = { WAIT_TIME }
-                placeholder = { placeholder }
                 type = 'text'
                 value = { value }
                 onChange = { handleChangeFunc }
